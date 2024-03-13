1483cb2c1f3b2a53f39b76a1a9a382f297adadff
Perfy <joy.okunzuwa@iisysgroup.com>
2024-03-13 13:06:53 +0100

    react-dic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, useState } from "react";
+import Axios from "axios";
+import "./App.css";
+import { FaSearch } from "react-icons/fa";
+import { FcSpeaker } from "react-icons/fc";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App (){
+  //setting up the initail state using react hook 'use state'
+
+  const [data, setData] = useState("");
+  const [searchWord, setSearchWord] = useState("");
+
+ //fucntion to fetch information on button
+ //click, set the data accordingly
+
+  function getMeaning() {
+    Axios.get(
+      `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`
+    ).then((response) => {
+      setData(response.data[0]);
+    });
+  }
+
+  //function to play and listen the phonetics of the searched 
+
+  function playAudio() {
+    console.log(data.phonetics)
+    let audioSelect = data.phonetics[0].audio;
+    if (audioSelect === ""){
+      audioSelect = data.phonetics[1].audio
+    }
+
+    let audio = new Audio(audioSelect);
+    audio.play();
+  }
+
+      return(
+        <div className="App">
+             <h1>Free Dictionary</h1>
+               <div className="searchBox">
+  
+                  {/* Taking user input */}
+                  <input
+                    type="text"
+                    placeholder="Search..."
+                    onChange={(e) => {
+                      setSearchWord(e.target.value);
+                    }}
+                  />
+                  <button
+                    onClick={() => {
+                      getMeaning();
+                    }}
+                  >
+                    <FaSearch size="20px" />
+                  </button>
+               </div>
+          {data && (
+              <div className="showResults">
+                      <h2>
+                        {data.word}{" "}
+                        <button
+                          onClick={() => {
+                            playAudio();
+                          }}
+                        >
+                          <FcSpeaker size="26px" />
+                        </button>
+                      </h2>
+                      <h4>Parts of speech:</h4>
+        
+                  
+                  <p>{data.meanings[0].partOfSpeech}</p>
+        
+        
+                  <h4>Definition:</h4>
+        
+                  
+                  <p>{data.meanings[0].definitions[0].definition}</p>
+        
+        
+                  <h4>Example:</h4>
+        
+                  
+                  <p>{data.meanings[0].definitions[0].example}</p>
+ 
+                </div>
+            )}
+        </div>
+  
+      );
+
+
 }
-
 export default App;
+
